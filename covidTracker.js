2d8129da3e48373973371072f995e6d3b8f6fca6
euvtjain <euvtjain@amway.com>
2021-05-24 21:07:36 +0530

    url tele

--- covidTracker.js
+++ covidTracker.js
@@ -12,40 +12,37 @@ if (process.env.NODE_ENV === "production") {
     "mongodb+srv://tushar:jain123@cowincluster.0ib1s.mongodb.net/cowin?retryWrites=true&w=majority";
 } else {
   shareURL =
-    "http://t.me/share/url?url=https://t.me/tusharjain352_cowin_slot_bot&text=Covid-19";
+    "https://t.me/share/url?url=https://t.me/tusharjain352_cowin_slot_bot&text=Covid-19";
 }
 
 const covidTrackerScene = new Scenes.WizardScene(
   "Covid_19_Tracker",
   async (ctx) => {
     try {
-      // if (ctx && ctx.message) {
-      //   const userData = {
-      //     first_name: ctx.message.from.first_name,
-      //     last_name: ctx.message.from.last_name,
-      //     user_id: ctx.message.from.id,
-      //   };
+      if (ctx && ctx.message) {
+        const userData = {
+          first_name: ctx.message.from.first_name,
+          last_name: ctx.message.from.last_name,
+          user_id: ctx.message.from.id,
+        };
 
-      //   const checkUserExist = await userService.checkUser(userData);
-      //   if (!checkUserExist) {
-      //     const saveUser = await userService.createUser(userData);
-      //   }
-      // }
+        const checkUserExist = await userService.checkUser(userData);
+        if (!checkUserExist) {
+          const saveUser = await userService.createUser(userData);
+        }
+      }
 
       ctx.reply(
         `Hi! ${ctx.from.first_name} 👋 \nWelcome to Covid Assistance bot. Here is what I can: \n \n -Search for vaccine slots 🇮🇳 \n -Available 🇮🇳 Vaccine's Information \n -Covid-19 🇮🇳 Tracker & WHO links \n \n Shall we start? 👇 `,
         Markup.inlineKeyboard(
           [
             Markup.button.callback("Search 💉 Slots ", "VACCINE"),
-            Markup.button.url("Covid-19 🇮🇳 IN", "http://www.covid19india.org/"),
-            Markup.button.url("WHO 🌎", "http://covid19.who.int/"),
             Markup.button.url(
-              "🌎 Vaccine Tracker",
-              "http://www.bing.com/covid/local/india?vert=vaccineTracker"
+              "Covid-19 🇮🇳 IN",
+              "https://t.me/share/url?url=https://www.covid19india.org/"
             ),
             Markup.button.callback("Available 💉 Vaccine's 🇮🇳", "VACCINE_INFO"),
             Markup.button.callback("EXIT 🚪", "CANCELLED"),
-            Markup.button.url("Share Now 👫", shareURL),
           ],
           { columns: 2 }
         )
@@ -71,7 +68,7 @@ const covidTrackerScene = new Scenes.WizardScene(
       }
     } catch (e) {
       console.log("Exception in covid tracker scene", e);
-      // return ctx.scene.enter("Default_Error");
+      return ctx.scene.enter("Default_Error");
     }
   }
 );
